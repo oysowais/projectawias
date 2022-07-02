9948aee60392fa4a8023f391ab8d763ea8d1e67a
GS <GS@DESKTOP-SLCRNMN>
2022-07-02 23:28:02 +0500

    Add project files.

---
+++ assignment2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace assignment2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
